effe3ab4f0d2ea7a4bc0814feb68d1b47683e567
Tomek Polanski <polanski.tomek@gmail.com>
2019-07-22 20:00:28 +0200

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:animation_cheat_page/animations_cheat_sheet.dart';
-import 'package:flutter/foundation.dart'
-    show debugDefaultTargetPlatformOverride;
-import 'package:flutter/material.dart';
+import 'package:animation_cheat_page/shared/material_import.dart';
 
 void main() {
   debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
